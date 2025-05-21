64fdc983016eaf7adba1e881f7b68ad4ffcbf59e
Irina Saf <132776531+rabbitrunout@users.noreply.github.com>
2025-05-21 14:38:20 -0400

    Added header and footer

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>Contact Manager - Home</title>
 </head>
 <body>
-    
+    <?php include ("header.php"); ?>
+
+    <main>
+        <h2>Contact List</h2>
+    </main>
+
+    <?php include ("footer.php"); ?>
 </body>
 </html>
